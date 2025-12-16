2fd0c23eb7ae817b17a1a1d617fc806bcdb88269
Aufa <aufa_romeo@live.com>
2025-12-17 03:50:06 +0800

    FEAT: whoops. 1 more post service

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -5,3 +5,12 @@ export async function getAllPosts(): Promise<Post[]> {
   const res = await api.get<Post[]>("/posts");
   return res.data;
 }
+
+export async function createPost(post: {
+  title: string;
+  image: string;
+  coords: { longitude: number; latitude: number };
+}): Promise<Post> {
+  const res = await api.post<Post>("/posts", post, { authRequired: true });
+  return res.data;
+}
