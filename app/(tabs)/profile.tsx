d2a1fb536934c36096b408c48941e92847639508
Aufa <aufa_romeo@live.com>
2025-12-20 04:46:26 +0800

    FEAT: profile posts layout. delete icon. signout WIP

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,19 +1,30 @@
+import { useAuth } from "@/src/contexts/AuthContext";
+import { Ionicons } from "@expo/vector-icons";
 import { useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
 import {
   ActivityIndicator,
+  Alert,
+  Dimensions,
   FlatList,
   Image,
   Pressable,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { deletePost, getMyPosts } from "../../src/services/post.service";
 import { Post } from "../../src/types/post";
 
+
+const screenWidth = Dimensions.get("window").width;
+const cardMargin = 8;
+
 export default function Profile() {
+  const {signout} = useAuth();
+
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -34,6 +45,17 @@ export default function Profile() {
       loadMyPosts();
     }, [])
   );
+
+  const handleSignout = async () => {
+
+    try {
+      await signout();
+    } catch (err: any) {
+      Alert.alert("Clean Signout Failed", err?.response?.data?.error ?? err.message);
+    } finally {
+      // router.replace("/");
+    }
+  }
 
   async function onDelete(postId: string) {
     try {
@@ -61,32 +83,42 @@ export default function Profile() {
   }
 
   return (
-    <SafeAreaView style={styles.safeView}>
+    <SafeAreaView style={styles.safeView} edges={["top"]}>
       <FlatList
         data={posts}
         keyExtractor={(item) => item._id}
+        numColumns={2}
         contentContainerStyle={styles.list}
         renderItem={({ item }) => (
           <View style={styles.card}>
             <Image source={{ uri: item.image }} style={styles.image} />
 
+            <Pressable style={styles.deleteButton} onPress={() => onDelete(item._id)}>
+              <Ionicons name="trash" color="red"/>
+            </Pressable>
+
             <View style={styles.row}>
               <Text style={styles.title}>{item.title}</Text>
-
-              <Pressable onPress={() => onDelete(item._id)}>
-                <Text style={styles.delete}>Delete</Text>
-              </Pressable>
             </View>
           </View>
         )}
       />
+      <View style={styles.signoutContainer}>
+        <TouchableOpacity style={styles.signoutButton} onPress={handleSignout}>
+          <Text style={styles.signoutText}>
+            SIGN OUT
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   safeView: {
+    // flexDirection: "row",
     flex: 1,
+    backgroundColor: "white",
   },
   center: {
     flex: 1,
@@ -100,8 +132,9 @@ const styles = StyleSheet.create({
   },
 
   list: {
+    // flex: 1,
     padding: 12,
-    backgroundColor: "#000",
+    backgroundColor: "#ffffffff",
   },
 
   card: {
@@ -109,6 +142,8 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     overflow: "hidden",
     backgroundColor: "#111",
+    marginHorizontal: cardMargin,
+    width: (screenWidth / 2) - (cardMargin * 3),
   },
 
   image: {
@@ -129,8 +164,28 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
 
-  delete: {
-    color: "#ff3b30",
-    fontWeight: "600",
+  deleteButton: {
+    position: "absolute",
+    right: 0,
+    margin: 10,
   },
+
+  signoutContainer: {
+    padding: 10,
+    backgroundColor: "#898989ff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signoutButton: {
+    paddingHorizontal: 50,
+    paddingVertical: 15,
+    backgroundColor: "#ff0000ff",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signoutText: {
+    fontWeight: 800,
+    color: "#ffffff"
+  }
 });
